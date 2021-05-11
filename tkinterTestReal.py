dbfefefe4cada82ded703ed1916a94f9a590b1bd
kevinthepapaya <79876520+kevinthepapaya@users.noreply.github.com>
2021-05-11 11:46:16 -0600

    Add files via upload

--- tkinterTestReal.py
+++ tkinterTestReal.py
@@ -9,10 +9,7 @@ def results():
     print(groceryList)
    
 
-def addToList():
-    newItem = E1.get()
-    groceryList.append(newItem)
-    E1.delete(0, END)
+
 
 def exportList():
     with open('test.txt', 'w') as f:
@@ -29,15 +26,20 @@ def mainMenu():
     LMain.grid(column = 0, row = 0)
     B1Main = Button(text ="  Week 1  ", bg= "blue", command = week1)
     B1Main.grid(colum = 0, row =1)
-    B2Main = Button(text ="  Week 2  ", bg= "blue")
+    B2Main = Button(text ="  Week 2  ", bg= "blue", command = week2)
     B2Main.grid(colum =0, row =2)
-    B3Main = Button(text ="  Week 3  ", bg= "blue")
+    B3Main = Button(text ="  Week 3  ", bg= "blue",)
     B3Main.grid(colum =0, row =3)
 
 """"
 export doesn't work and window doesn't show up.
 """"
 def week1():
+    
+    def addToList():
+        newItem = E1.get()
+        groceryList.append(newItem)
+        E1.delete(0, END)
     clearWindow()    
     #This is a Label widget
     L1 = Label(top, text="Grocery List")
@@ -57,10 +59,33 @@ def week1():
     B3 = Button(text = "Export List", bg= "red", command = exportList)
     B3.grid(column = 2, row = 3)
 
-    #Bclear = Button( text = "Clear Window", bg = "blue", command = clearWindow)
-    #Bclear.grid(column = 3, row =3)
+    Bclear = Button( text = "Main Menu", bg = "blue", command = mainMenu)
+    Bclear.grid(column = 3, row =3)
 
-    #mainloop is an initialization statement, a way to start. But for tkinter, this statement has to be the last line
+    mainloop is an initialization statement, a way to start. But for tkinter, this statement has to be the last line
+
+def week2():
+    clearWindow()
+    L1W2 = Label(top, text= "Dice Roller Program")
+    L1W2.grid(column =3 , row =0 )
+    
+    L2W2 = Label(top, text= "How many sides?")
+    L2W2.grid(column =1 , row =3 )
+    
+    L3W2 = Label (text = "How many rolls?")
+    L3W2.grid(column = 5, row = 3)
+    
+    E1W2 = Entry(top,bd=5)
+    E1W2.grid(column = 1, row =4 )
+    
+    E2W2 = Entry(top,bd=5)
+    E2W2.grid(column = 5, row =4 )
+    
+    B1W2 = Button(top, text = "Roll dice!!", bg = 'green', command = rolldice)
+    B1W2.grid(column = , row = )
+
+    #to add: roll function and exit button
+
 if __name__=="__main__":
     mainMenue()
     top.mainloop()
